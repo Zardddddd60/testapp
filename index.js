2496eb33a73194d204d9bff94822541350be08f1
竹釉 <ld233998@alibaba-inc.com>
2021-04-12 20:37:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const testpackage_1 = require("testpackage");
+testpackage_1.fn();
+const a = 1;
+console.log(a);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,8 @@
+import { fn } from 'testpackage';
+
+fn();
+
+
+const a = 1;
+
+console.log(a);
